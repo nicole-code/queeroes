e05b354c13259b9e035d398816e8afe3b4b30bed
Nicole Stawikowski <nicole.stawikowski@gmail.com>
2021-07-06 22:52:04 -0400

    implement CRUD functionality Django

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('index/', views.queero_index, name='index'),
     path('queeroes/<int:queero_id>/', views.queero_detail, name='detail'),
-
+    path('queeroes/new', views.queero_new, name='newqueero'),
+    path('queeroes_submit/', views.queero_create, name='createqueero'),
+    path('queeroes/<int:queero_id>/delete/', views.queero_delete, name='deletequeero'),
+    path('queeroes/<int:queero_id>/edit/', views.queero_edit, name='editqueero'),
+    path('queeroes/<int:queero_id>/submit_update_form/', views.queero_update, name='updatequeero'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Add the following import
 from django.http import HttpResponse
@@ -6,7 +6,7 @@ from .models import Queero
 
 # Define the home view
 def home(request):
-    return HttpResponse('<h1>Thats So Gay</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
@@ -19,7 +19,32 @@ def queero_detail(request, queero_id):
     queero = Queero.objects.get(id=queero_id)
     return render(request, 'queeroes.html', {'queeroes' : queero})  
 
+def queero_new(request):
+    return render(request, 'new.html')    
 
+def queero_create(request):
+    queero = Queero.objects.create(
+        name=request.POST['name'],
+        occupation=request.POST['occupation'],
+        biography=request.POST['biography'],
+        legacy=request.POST['legacy'],
+    )
+    return redirect(f'/queeroes/{queero.id}')
 
+def queero_delete(request, queero_id):
+    queero = Queero.objects.get(id=queero_id)    
+    queero.delete()
+    return redirect('/index')
 
+def queero_edit(request, queero_id):
+    queero = Queero.objects.get(id=queero_id)  
+    return render(request, 'edit.html', {'queeroes' : queero}) 
 
+def queero_update(request, queero_id):    
+    queero = Queero.objects.get(id=queero_id)  
+    queero.name = request.POST['name']
+    queero.occupation = request.POST['occupation']
+    queero.biography = request.POST['biography']
+    queero.legacy= request.POST['legacy']
+    queero.save()
+    return redirect(f'/queeroes/{queero.id}')
